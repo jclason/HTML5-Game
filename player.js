02e7423090368282617cb6368bbca574a02fc22b
Jared Clason <jclason1@gmail.com>
2011-11-02 20:07:14 -0400

    Made the ship in the About box explode when your ship in the game does, just for fun

--- player.js
+++ player.js
@@ -1,32 +1,33 @@
 function Player() {
 return {
-			x: 220,
-			y: 370,
-			width: 32,
-			height: 32,
-			rateOfFire: 250,
-			active: true,
-			charging: false,
-			draw: function() {
-				this.sprite.draw(canvas, this.x, this.y);
-			},
-			midpoint: function() {
-				return {
-					x: this.x + this.width/2,
-					y: this.y + this.height/2
-				};
-			},
-			shoot: function() {
-				var bulletPosition = this.midpoint();
-				
-				setTimeout(function() { 
-								player.charging = false;
-								playerBullets.push(Bullet({
-								speed: 7,
-								x: bulletPosition.x,
-								y: bulletPosition.y
-								}))}, this.rateOfFire);
-			},
+		x: 220,
+		y: 370,
+		width: 32,
+		height: 32,
+		rateOfFire: 250,
+		active: true,
+		charging: false,
+		draw: function() {
+			this.sprite.draw(canvas, this.x, this.y);
+		},
+		midpoint: function() {
+			return {
+				x: this.x + this.width/2,
+				y: this.y + this.height/2
+			};
+		},
+		shoot: function() {
+			var bulletPosition = this.midpoint();
+			
+			setTimeout(function() { 
+							player.charging = false;
+							playerBullets.push(Bullet({
+							speed: 7,
+							x: bulletPosition.x,
+							y: bulletPosition.y
+							}))}, this.rateOfFire);
+			return false;
+		},
 		
 		
 		sprite: Sprite("player.png"),
